bccde8c5a3965174636ad48356d2937544849813
Waldo van Paddenburg <waldovp@gmail.com>
2020-10-07 18:06:44 +1300

    init project and set boilerplate

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return <Text>Index Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
